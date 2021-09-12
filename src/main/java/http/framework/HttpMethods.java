6b8624c6bb795c2a34b5ce279d67e3eedea03a25
Андраник Вачаганович Григорян <53790991+MajorPayne36@users.noreply.github.com>
2021-09-12 19:29:48 +0300

    Initial commit

---
+++ src/main/java/http/framework/HttpMethods.java
@@ -0,0 +1,8 @@
+package http.framework;
+
+public class HttpMethods {
+  private HttpMethods() {}
+
+  public static final String GET = "GET";
+  public static final String POST = "POST";
+}
---
+++ src/main/java/http/framework/Request.java
@@ -0,0 +1,17 @@
+package http.framework;
+
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.Map;
+
+@Value
+@Builder
+public class Request {
+  String method;
+  String path;
+  // TODO: query
+  Map<String, String> headers;
+  @Builder.Default
+  byte[] body = new byte[]{};
+}
